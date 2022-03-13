fd67e418059a304e8cf93e133a28fb6d1d341ec5
STANOWISKO_D001 <uhulululu23@gmail.com>
2022-03-13 15:37:32 +0100

    ZADANIEE

--- src/Firma.java
+++ src/Firma.java
@@ -5,28 +5,52 @@ public class Firma {
     public static void main(String[] args) {
 
         ArrayList<Pracownik> lista_pracownikow = new ArrayList<>();
-        for(int i = 0; i<3;i++){
-            lista_pracownikow.add(new Pracownik());
-        }
-        for(Pracownik i : lista_pracownikow){
-            System.out.println(i.toString());
-        }
+
         Scanner input = new Scanner(System.in);
         String akcja = "1";
         while (!akcja.equals("stop")) {
-            System.out.println("wpisz 'stop' by zakończyć akcję");
+            System.out.println("wpisz 'add' aby dodać pracownika,'list' by pokazać listę pracowników, 'x' by zakończyć akcję");
             akcja = input.next();
-            if (akcja.equals("stop")) {
+            if (akcja.equals("x")) {
+
                 break;
             }
             switch (akcja) {
-                case"f":
+                case "add":
+                    lista_pracownikow.add(new Pracownik());
+                    break;
+                case "list":
+                    int i=0;
+                    for (Pracownik temp : lista_pracownikow) {
+                        System.out.print(i);
+                        System.out.println(temp.dane());
+                        i++;
+                    }
+                    break;
+                case "delete":
+                    System.out.println("Którego pracownika zwolnić? Wpisz jego numer id:");
+                    int ktoregoWywalic = input.nextInt();
+                    lista_pracownikow.remove(ktoregoWywalic);
 
                     break;
-                case"p":
+                case "suma":
+                    int wszystkiePlace = 0;
+                    for (Pracownik temp : lista_pracownikow){
+                        wszystkiePlace+=temp.getPlaca();
+                    }
+                    System.out.println(wszystkiePlace);
 
+                    break;
+                case "raise":
+                    System.out.println("Komu dać podwyżkę? Wpisz jego numer id:");
+                    int id = input.nextInt();
+                    System.out.println("Jaka ilość?");
+                    int kasa = input.nextInt();
+                    lista_pracownikow.get(id).podwyzka(kasa);
+                    break;
+            }
 
         }
-
     }
 }
+
--- src/Pracownik.java
+++ src/Pracownik.java
@@ -8,39 +8,40 @@ public class Pracownik {
     public Pracownik() {
         Scanner input = new Scanner(System.in);
             System.out.print("Podaj Imię i Nazwisko:");
-            Imię_i_Nazwisko = input.next();
+            this.Imię_i_Nazwisko = input.next();
             System.out.print("Podaj Adres:");
-            Adres = input.next();
-            System.out.println("Podaj Płacę:");
+            this.Adres = input.next();
+            System.out.print("Podaj Płacę:");
             this.placa = input.nextInt();
     }
 
     public String getImię_i_Nazwisko() {
-        return Imię_i_Nazwisko;
+        return this.Imię_i_Nazwisko;
     }
 
     public void setImię_i_Nazwisko(String imię_i_Nazwisko) {
-        Imię_i_Nazwisko = imię_i_Nazwisko;
+        this.Imię_i_Nazwisko = imię_i_Nazwisko;
     }
 
     public String getAdres() {
-        return Adres;
+        return this.Adres;
     }
 
     public void setAdres(String adres) {
-        Adres = adres;
+        this.Adres = adres;
     }
 
     public int getPlaca() {
-        return placa;
+        return this.placa;
     }
 
     public void setPlaca(int placa) {
         this.placa = placa;
     }
-
+    public void podwyzka(int placa){
+        this.placa += placa;
+    }
     public String dane() {
-        /String tekst = "Imię i Nazwisko: " + this.Imię_i_Nazwisko + " Adres: " + this.Adres + "Płaca: " + this.placa;
-        return tekst;
+        return "Imię i Nazwisko: " + this.Imię_i_Nazwisko + "\tAdres: " + this.Adres + "\tPłaca: " + this.placa;
     }
 }
