b310ae31dc2f825896a6cfd7f228bc1352781b20
STANOWISKO_D001 <uhulululu23@gmail.com>
2022-03-13 12:52:06 +0100

    ZADANIEE

--- src/Firma.java
+++ src/Firma.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class Firma {
+    public static void main(String[] args) {
+        ArrayList<Pracownik> lista_pracowników = new ArrayList<>();
+        for(int i = 0; i<3;i++){
+            lista_pracowników.add(new Pracownik());
+        }
+        for(Pracownik i : lista_pracowników){
+            System.out.println(i.toString());
+        }
+
+
+    }
 }
--- src/Pracownicy.java
+++ src/Pracownicy.java
@@ -1,2 +1,32 @@
-public class Pracownicy {
+public class Pracownik {
+    private String Imię_i_Nazwisko;
+    private String Adres;
+    private int placa;
+
+    public String getImię_i_Nazwisko() {
+        return Imię_i_Nazwisko;
+    }
+
+    public void setImię_i_Nazwisko(String imię_i_Nazwisko) {
+        Imię_i_Nazwisko = imię_i_Nazwisko;
+    }
+
+    public String getAdres() {
+        return Adres;
+    }
+
+    public void setAdres(String adres) {
+        Adres = adres;
+    }
+
+    public int getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(int placa) {
+        this.placa = placa;
+    }
+    public String toString(){
+        String tekst="Imię i Nazwisko: "+Imię_i_Nazwisko+" Adres: "+Adres+"Płaca: "+placa;
+        return tekst;
 }
