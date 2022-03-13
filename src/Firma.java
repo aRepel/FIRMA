560a7d6996d5152a21406814c97c44a875ec3221
STANOWISKO_D001 <uhulululu23@gmail.com>
2022-03-13 14:20:18 +0100

    ZADANIEE

--- src/Firma.java
+++ src/Firma.java
@@ -1,15 +1,32 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Firma {
     public static void main(String[] args) {
-        ArrayList<Pracownik> lista_pracowników = new ArrayList<>();
+
+        ArrayList<Pracownik> lista_pracownikow = new ArrayList<>();
         for(int i = 0; i<3;i++){
-            lista_pracowników.add(new Pracownik());
+            lista_pracownikow.add(new Pracownik());
         }
-        for(Pracownik i : lista_pracowników){
+        for(Pracownik i : lista_pracownikow){
             System.out.println(i.toString());
         }
+        Scanner input = new Scanner(System.in);
+        String akcja = "1";
+        while (!akcja.equals("stop")) {
+            System.out.println("wpisz 'stop' by zakończyć akcję");
+            akcja = input.next();
+            if (akcja.equals("stop")) {
+                break;
+            }
+            switch (akcja) {
+                case"f":
 
+                    break;
+                case"p":
+
+
+        }
 
     }
 }
--- src/Pracownik.java
+++ src/Pracownik.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Pracownik {
     private String Imię_i_Nazwisko;
     private String Adres;
     private int placa;
+
+    public Pracownik() {
+        Scanner input = new Scanner(System.in);
+            System.out.print("Podaj Imię i Nazwisko:");
+            Imię_i_Nazwisko = input.next();
+            System.out.print("Podaj Adres:");
+            Adres = input.next();
+            System.out.println("Podaj Płacę:");
+            this.placa = input.nextInt();
+    }
 
     public String getImię_i_Nazwisko() {
         return Imię_i_Nazwisko;
@@ -26,7 +38,9 @@ public class Pracownik {
     public void setPlaca(int placa) {
         this.placa = placa;
     }
-    public String toString(){
-        String tekst="Imię i Nazwisko: "+Imię_i_Nazwisko+" Adres: "+Adres+"Płaca: "+placa;
+
+    public String dane() {
+        /String tekst = "Imię i Nazwisko: " + this.Imię_i_Nazwisko + " Adres: " + this.Adres + "Płaca: " + this.placa;
         return tekst;
+    }
 }
